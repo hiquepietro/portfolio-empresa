c7f48c7268fd54a028058e79b1404d05717f5ac5
Hique Pietro <hiquepietro@gmail.com>
2024-07-09 12:26:18 -0300

    feat: configuraçao do portfolio

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function Button() {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>    
+    )
+}
